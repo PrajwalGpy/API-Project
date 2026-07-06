cd0f63267a5cce44354722f5cca14b3773773039
PrajwalGpy <prajwalgpy@gmail.com>
2026-07-06 14:06:54 +0530

    Added

--- tests/booking.spec.js
+++ tests/booking.spec.js
@@ -1,18 +1,60 @@
 import { test, expect, request } from "@playwright/test";
 
-test("Test login", async ({ request }) => {
- const response =  await request.post("https://restful-booker.herokuapp.com/auth", {
-    headers: { "Content-Type": "application/json" },
-    data: {
-      username: "admin",
-      password: "password123",
-    },
+let authToken;
+let bookingid;
+test.describe.configure({ mode: "serial" });
+
+test.describe("Authenticated API Tests", () => {
+  test.beforeAll(async ({ request }) => {
+    const response = await request.post(
+      "https://restful-booker.herokuapp.com/auth",
+      {
+        headers: { "Content-Type": "application/json" },
+        data: {
+          username: "admin",
+          password: "password123",
+        },
+      },
+    );
+
+    const body = await response.json();
+    authToken = body.token;
   });
 
-  expect(response.ok()).toBeTruthy()
-  const body = await response.json()
-  const token = body.token
-  console.log(token)
+  test("Creaate Booking info", async ({ request }) => {
+    const response = await request.post(
+      "https://restful-booker.herokuapp.com/booking",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          firstname: "Prajwal",
+          lastname: "G P",
+          totalprice: 300,
+          depositpaid: true,
+          bookingdates: {
+            checkin: "2026-01-01",
+            checkout: "2026-02-01",
+          },
+          additionalneeds: "Breakfast",
+        },
+      },
+    );
+
+    expect(response.ok()).toBeTruthy();
+
+    const data = await response.json();
+    bookingid = data.bookingid;
+    console.log(bookingid);
+  });
+
+  test("Get Bppking  info", async ({ request }) => {
+    const response = await request.get(
+      "https://restful-booker.herokuapp.com/booking/" + bookingid,
+    );
+    const body = await response.json();
+    expect(response.ok()).toBeTruthy();
+    expect(body.firstname).toBe("Prajwal");
+  });
 });
-
-
