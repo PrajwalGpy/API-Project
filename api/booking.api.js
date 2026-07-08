f6cf38f69bb67c1138f1cbd09c2f5cb78139cf9a
PrajwalGpy <prajwalgpy@gmail.com>
2026-07-08 09:15:08 +0530

    apii created

---
+++ api/booking.api.js
@@ -0,0 +1,13 @@
+import { sendRequest } from "../utility/apiClients";
+
+createBooking = async (requestContext, payload, token) => {
+  return sendRequest(requestContext, "post", "/booking", payload, token);
+};
+
+async function getBooking(requestContext, id, token) {
+  return sendRequest(requestContext, "get", `/booking/${id}`, null, token);
+}
+
+async function updateBooking(requestContext, id, payload, token) {
+  return sendRequest(requestContext, "put", `/booking/${id}`, payload, token);
+}
--- utility/apiClients.js
+++ utility/apiClients.js
@@ -1,21 +1,33 @@
 async function sendRequest(requestContext, method, url, data, token) {
-  const headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  };
+    const headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    };
 
-  if (token) {
-    headers.Authorization = `Bearer ${token}`;
-    headers.Cookie = `token=${token}`;
-  }
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
+        headers.Cookie = `token=${token}`;
+    }
 
-  const requestOptions = { headers };
+    const requestOptions = { headers };
 
-  if (
-    ["put", "post", "get"].includes(method) &&
-    data !== null &&
-    data !== undefined
-  ) {
-    requestOptions.data = data;
-  }
+    if (
+        ["put", "post", "get"].includes(method) &&
+        data !== null &&
+        data !== undefined
+    ) {
+        requestOptions.data = data;
+    }
+
+    const response = await requestContext[method](url, requestOptions);
+
+    const body = await response.json().catch(() => null);
+
+    return {
+        status: response.status(),
+        body: body,
+        headers: response.headers(),
+    };
 }
+
+module.exports = { sendRequest }
