dc808af62a76cac69f4e7a37ada5d3e35211ff49
PrajwalGpy <prajwalgpy@gmail.com>
2026-07-08 08:59:52 +0530

    adding new api

---
+++ utility/apiClients.js
@@ -0,0 +1,21 @@
+async function sendRequest(requestContext, method, url, data, token) {
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+    headers.Cookie = `token=${token}`;
+  }
+
+  const requestOptions = { headers };
+
+  if (
+    ["put", "post", "get"].includes(method) &&
+    data !== null &&
+    data !== undefined
+  ) {
+    requestOptions.data = data;
+  }
+}
